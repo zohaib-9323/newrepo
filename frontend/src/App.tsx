664b79349d00e9746538333f478a61004e4470fe
zohaib <mzohaib0677@gmail.com>
2024-11-07 14:23:12 +0500

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,7 +32,8 @@ const App: React.FC = () => {
 
   const handleLogin = async (email: string, password: string) => {
     try {
-      const BASE_URL=`${process.env.REACT_APP_PUBLIC_URL}auth/login`;
+      // const BASE_URL=`${process.env.REACT_APP_PUBLIC_URL}auth/login`;
+      const BASE_URL='http://localhost:5007/auth/login'
       const response = await fetch(BASE_URL, {
         method: 'POST',
         headers: {
