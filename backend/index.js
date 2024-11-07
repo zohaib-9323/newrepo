1945a9e5bd01a19fb47fa26ac20da962d7ef7103
zohaib <mzohaib0677@gmail.com>
2024-11-07 16:25:11 +0500

    Fixing

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,7 @@ const TeacherRouter = require('./Routes/TeacherRouter');
 
 const app = express();
 const PORT = process.env.PORT || 5007;
-app.use(cors({
-  origin: ['http://localhost:5007', 'https://schoolmanagementsystem-mzohaib0677-gmailcoms-projects.vercel.app'],
-  credentials: true, // Enable cookies or authorization headers
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-}));
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.json());
 app.use('/auth',AuthRouter);
