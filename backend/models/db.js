0603459887d0763cd15443685e0694723c9ecb71
zohaib <mzohaib0677@gmail.com>
2024-11-07 12:16:57 +0500

    env variable

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const MONGODB_URI = 'mongodb+srv://mzohaib0677:KcPAAXmFtfJqEAUA@cluster0.0hgzd.mongodb.net/CourseManagement?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.REACT_APP_MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => {
     console.log("Connected to MongoDB");
+
 })
 .catch((error) => {
     console.error("MongoDB connection error:", error);
+
 });
