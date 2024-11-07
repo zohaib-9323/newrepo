7f0af04dc045c4d63f7b27eb853da8e6eca12d7c
zohaib <mzohaib0677@gmail.com>
2024-11-07 12:30:36 +0500

    Update env

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-mongoose.connect(process.env.REACT_APP_MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_SECRET, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => {
     console.log("Connected to MongoDB");
-
 })
 .catch((error) => {
     console.error("MongoDB connection error:", error);
-
 });
