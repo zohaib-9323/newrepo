db5ec9eb60e33a99faf3a1572bdbbe0a298a9cef
zohaib <mzohaib0677@gmail.com>
2024-11-07 15:25:40 +0500

    test6

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,7 @@ const TeacherRouter = require('./Routes/TeacherRouter');
 
 const app = express();
 const PORT = process.env.PORT || 5007;
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:3001/' || 'http://localhost:3000/',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-};
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use('/auth',AuthRouter);
