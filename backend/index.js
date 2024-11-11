9c2c56a0466bd0259be8f3663941b0335a2b7324
zohaib <mzohaib0677@gmail.com>
2024-11-11 17:33:00 +0500

    commit123

--- backend/index.js
+++ backend/index.js
@@ -12,16 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5007;
 
 
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true, 
-}));
-
-
-app.options('*', cors());
-app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
 
 // Define routes
 app.use('/auth', AuthRouter);
